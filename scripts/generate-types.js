d0949c208a4e0b429fc45e304a2f841014b3c412
Igor Klimer <github@gpg.klimer.eu>
2025-05-28 18:11:46 +0000

    Use Date type for datetime OpenAPI fields
    Fixes https://github.com/OpusDNS/opusdns-api/issues/1062
    
    datetime fields are treated like strings in the generated types, which feels like a waste. Apparently it's by design (since the projects using these types could be using different date/time types): openapi-ts/openapi-typescript#2237
    
    Looks like our dashboard is simply using vanilla Date, so same as for TypeID, we should use transform to force that type.
    
    Code based on https://openapi-ts.dev/node#transform-posttransform

--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -1,7 +1,20 @@
 import fs from "node:fs";
+import ts from "typescript";
 import openapiTS, { astToString } from "openapi-typescript";
 
-const ast = await openapiTS(new URL("../src/openapi.json", import.meta.url));
+const DATE = ts.factory.createTypeReferenceNode(ts.factory.createIdentifier("Date"));
+const NULL = ts.factory.createLiteralTypeNode(ts.factory.createNull());
+
+const schemaPath = new URL("../src/openapi.json", import.meta.url)
+const ast = await openapiTS(schemaPath, {
+	transform(schemaObject, metadata) {
+		if (schemaObject.format === "date-time") {
+			return schemaObject.nullable
+				? ts.factory.createUnionTypeNode([DATE, NULL])
+				: DATE;
+		}
+	},
+});
 const contents = astToString(ast);
 
 fs.writeFileSync("./src/schema.d.ts", contents);
