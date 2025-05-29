9fb76003321216c9fb190d5e0f9e14f4499a3be8
Igor Klimer <github@gpg.klimer.eu>
2025-05-29 12:27:08 +0000

    Improve TypeID support
    Fixes https://github.com/OpusDNS/opusdns-api/issues/1034
    Related: https://github.com/OpusDNS/opusdns-api/pull/1068
    
    This puts concrete types for TypeID, ensuring the wrong type cannot
    be used. This is especially useful for crafting requests for the backend
    using the fetch library, ensuring the correct ID is being sent.
    Previously, they were all typed as strings, making a mistake very easy.

--- scripts/generate-types.js
+++ scripts/generate-types.js
@@ -5,15 +5,47 @@ import openapiTS, { astToString } from "openapi-typescript";
 const DATE = ts.factory.createTypeReferenceNode(ts.factory.createIdentifier("Date"));
 const NULL = ts.factory.createLiteralTypeNode(ts.factory.createNull());
 
+function getTypeIdPrefix(schemaObject) {
+	if (! "format" in schemaObject) {
+		return null;
+	}
+	if (! "x-typeid-prefix" in schemaObject) {
+		return null;
+	}
+
+	return schemaObject["x-typeid-prefix"];
+}
+
 const schemaPath = new URL("../src/openapi.json", import.meta.url)
 const ast = await openapiTS(schemaPath, {
 	transform(schemaObject, metadata) {
-		if (schemaObject.format === "date-time") {
-			return schemaObject.nullable
-				? ts.factory.createUnionTypeNode([DATE, NULL])
-				: DATE;
+		switch (schemaObject.format) {
+			case "date-time":
+				return schemaObject.nullable
+					? ts.factory.createUnionTypeNode([DATE, NULL])
+					: DATE;
+
+			case "typeid":
+				const typeIdPrefix = getTypeIdPrefix(schemaObject);
+				if (!typeIdPrefix) {
+					return null;
+				}
+
+				const typeIdType = ts.factory.createTypeReferenceNode(
+					ts.factory.createIdentifier("TypeID"),
+					[
+						ts.factory.createLiteralTypeNode(
+							ts.factory.createStringLiteral(typeIdPrefix)
+						)
+					]
+				);
+				return typeIdType;
+
+			default:
+				return null;
 		}
 	},
+	inject: "import { TypeID } from 'typeid-js';",
 });
 const contents = astToString(ast);
 
